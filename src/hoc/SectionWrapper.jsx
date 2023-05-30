d251e1640be6af871835100bd29c18e078161634
Jonathan <Zweitonegoismus@gmail.com>
2023-05-30 13:54:07 -0400

    Project cards section complete.

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,12 +6,11 @@ import { staggerContainer } from '../utils/motion';
 const SectionWrapper = (Component, idName) => function HOC() {
     return (
         <motion.section
-            variansts={staggerContainer()}
+            variants={staggerContainer()}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true, amount: 0.25 }}
-            className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-        >
+            className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
             <span className="hash-span" id={idName}>
                 &nbsp;
             </span>
